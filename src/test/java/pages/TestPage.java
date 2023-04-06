2a3aeda28091776ac8dd4de84a559d197e8eb49c
keskinus <ummkeskin@gmail.com>
2023-04-06 10:27:59 -0700

    first commit

---
+++ src/test/java/pages/TestPage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+public class TestPage {
+    // This is my TestCenter page class
+    // All objects ib TestCenter page will go here
+
+//    public SelenideElement username(){
+//        return $(By.id("exampleInputEmail1"));
+//    }
+//     OR ALTERNATIVELY
+//      WebElement ->SelenideElement
+    public SelenideElement username = $(By.id("exampleInputEmail1"));
+
+    public SelenideElement password = $(By.id("exampleInputPassword1"));
+
+    public SelenideElement submitButton = $(By.xpath("//button[@type='submit']"));
+
+}
---
+++ target/test-classes/features/02_locators.feature
@@ -0,0 +1,8 @@
+@locators
+Feature: locators feature
+  Scenario: TC01_locators
+    Given I navigate to "https://testcenter.techproeducation.com/index.php?page=form-authentication"
+    And I enter username
+    And I enter password
+    And I enter submit button
+
